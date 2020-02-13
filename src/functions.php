18d02c61f4f2897b264c4110f67516ae15775ee4
Mistralys <s.mordziol@gmail.com>
2020-02-13 21:36:51 +0100

    Added localization support functions.

---
+++ src/functions.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Mailcode;
+
+/**
+ * Translation function used to translate some of the internal
+ * strings: if the localization is installed, it will use this
+ * to do the translation.
+ * 
+ * @return string
+ */
+function t()
+{
+    $args = func_get_args();
+    
+    return call_user_func_array('\AppLocalize\t', $args);
+}
+
+/**
+ * Initializes the utilities: this is called automatically
+ * because this file is included in the files list in the
+ * composer.json, guaranteeing it is always loaded.
+ */
+function init()
+{
+    if(!class_exists('\AppLocalize\Localization')) {
+        return;
+    }
+    
+    $installFolder = realpath(__DIR__.'/../');
+    
+    // Register the classes as a localization source,
+    // so they can be found, and use the bundled localization
+    // files.
+    \AppLocalize\Localization::addSourceFolder(
+        'mailcode',
+        'Mailcode Syntax Parser',
+        'Composer Packages',
+        $installFolder.'/localization',
+        $installFolder.'/src'
+    );
+}
+
+init();
