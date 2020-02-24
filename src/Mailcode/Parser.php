2501d9cdbd66f956cab71b916feebef0f6dcd3fe
Mistralys <s.mordziol@gmail.com>
2020-02-24 14:24:35 +0100

    Added createSafeguard(); Internal cleanup.

--- src/Mailcode/Parser.php
+++ src/Mailcode/Parser.php
@@ -31,9 +31,15 @@ class Mailcode_Parser
     */
     protected $mailcode;
     
+   /**
+    * @var Mailcode_Commands
+    */
+    protected $commands;
+    
     public function __construct(Mailcode $mailcode)
     {
         $this->mailcode = $mailcode;
+        $this->commands = $this->mailcode->getCommands();
     }
     
    /**
@@ -47,7 +53,9 @@ class Mailcode_Parser
     }
     
    /**
-    * Parses a string to detect all commands within.
+    * Parses a string to detect all commands within. Returns a
+    * collection instance that contains information on all the 
+    * commands.
     * 
     * @param string $string
     * @return Mailcode_Collection A collection with all unique commands found.
@@ -59,9 +67,69 @@ class Mailcode_Parser
         $matches = array();
         preg_match_all(self::getRegex(), $string, $matches, PREG_PATTERN_ORDER);
         
-        $commands = $this->mailcode->getCommands();
-        
         $total = count($matches[0]);
+        
+        for($i=0; $i < $total; $i++)
+        {
+            $match = $this->parseMatch($matches, $i);
+            
+            $this->processMatch($match, $collection);
+        }
+        
+        return $collection;
+    }
+    
+   /**
+    * Processes a single match found in the string: creates the command,
+    * and adds it to the collection if it's a valid command, or to the list
+    * of invalid commands otherwise.
+    * 
+    * @param Mailcode_Parser_Match $match
+    * @param Mailcode_Collection $collection
+    */
+    protected function processMatch(Mailcode_Parser_Match $match, Mailcode_Collection $collection) : void
+    {
+        $name = $match->getName();
+        
+        if(!$this->commands->nameExists($name))
+        {
+            $collection->addErrorMessage(
+                $match->getMatchedString(),
+                t('No command found with the name %1$s.', $name),
+                Mailcode_Commands_Command::VALIDATION_UNKNOWN_COMMAND_NAME
+            );
+            return;
+        }
+        
+        $cmd = $this->commands->createCommand(
+            $this->commands->getIDByName($name),
+            $match->getType(),
+            $match->getParams(),
+            $match->getMatchedString()
+        );
+        
+        if($cmd->isValid())
+        {
+            $collection->addCommand($cmd);
+            return;
+        }
+        
+        $collection->addInvalidCommand($cmd);
+    }
+    
+   /**
+    * Parses a single regex match: determines which named group
+    * matches, and retrieves the according information.
+    * 
+    * @param array $matches The regex results array.
+    * @param int $index The matched index.
+    * @return Mailcode_Parser_Match
+    */
+    protected function parseMatch(array $matches, int $index) : Mailcode_Parser_Match
+    {
+        $name = ''; // the command name, e.g. "showvar"
+        $type = '';
+        $params = '';
         
         // 1 = single command
         // 2 = parameter command, name
@@ -70,59 +138,40 @@ class Mailcode_Parser
         // 5 = parameter type command, type
         // 6 = parameter type command, params
         
-        for($i=0; $i < $total; $i++)
+        if(!empty($matches[1][$index]))
         {
-            $name = ''; // the command name, e.g. "showvar"
-            $type = '';
-            $params = '';
-            $matched = $matches[0][$i]; // the exact matched string including spacing
-            
-            if(!empty($matches[1][$i])) 
-            {
-                $name = $matches[1][$i];
-            }
-            else if(!empty($matches[2][$i])) 
-            {
-                $name = $matches[2][$i];
-                $params = $matches[3][$i];
-            }
-            else if(!empty($matches[4][$i]))
-            {
-                $name = $matches[4][$i];
-                $type = $matches[5][$i];
-                $params = $matches[6][$i];
-            }
-            
-            $name = strtolower($name);
-            $type = strtolower($type);
-            $params = trim($params);
-            
-            if(!$commands->nameExists($name)) 
-            {
-                $collection->addErrorMessage(
-                    $matched,
-                    t('No command found with the name %1$s.', $name),
-                    Mailcode_Commands_Command::VALIDATION_UNKNOWN_COMMAND_NAME
-                );
-                continue;
-            }
-            
-            $cmd = $commands->createCommand(
-                $commands->getIDByName($name),
-                $type,
-                $params,
-                $matched
-            );
-            
-            if(!$cmd->isValid()) 
-            {
-                $collection->addInvalidCommand($cmd);
-                continue;
-            }
-            
-            $collection->addCommand($cmd);
+            $name = $matches[1][$index];
+        }
+        else if(!empty($matches[2][$index]))
+        {
+            $name = $matches[2][$index];
+            $params = $matches[3][$index];
+        }
+        else if(!empty($matches[4][$index]))
+        {
+            $name = $matches[4][$index];
+            $type = $matches[5][$index];
+            $params = $matches[6][$index];
         }
         
-        return $collection;
+        return new Mailcode_Parser_Match(
+            $name, 
+            $type, 
+            $params, 
+            $matches[0][$index]
+        );
+    }
+    
+   /**
+    * Creates an instance of the safeguard tool, which
+    * is used to safeguard commands in a string with placeholders.
+    * 
+    * @param string $subject The string to use to safeguard commands in.
+    * @return Mailcode_Parser_Safeguard
+    * @see Mailcode_Parser_Safeguard
+    */
+    public function createSafeguard(string $subject) : Mailcode_Parser_Safeguard
+    {
+        return new Mailcode_Parser_Safeguard($this, $subject);
     }
 }
