1a0393cc7a3cc58c963226e91d6a51fe0d792fa9
Mistralys <s.mordziol@gmail.com>
2020-02-24 14:24:57 +0100

    Added class for command string matches.

---
+++ src/Mailcode/Parser/Match.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * File containing the {@see Mailcode_Parser} class.
+ *
+ * @package Mailcode
+ * @subpackage Parser
+ * @see Mailcode_Parser
+ */
+
+declare(strict_types=1);
+
+namespace Mailcode;
+
+/**
+ * Mailcode parser match, container for a command found
+ * while parsing a string.
+ *
+ * @package Mailcode
+ * @subpackage Parser
+ * @author Sebastian Mordziol <s.mordziol@mistralys.eu>
+ */
+class Mailcode_Parser_Match
+{
+    protected $name;
+    
+    protected $type;
+    
+    protected $params;
+    
+    protected $matchedString;
+    
+    public function __construct(string $name, string $type, string $params, string $matchedString)
+    {
+        $this->name = strtolower($name);
+        $this->type = strtolower($type);
+        $this->params = trim($params);
+        $this->matchedString = $matchedString;
+    }
+    
+    public function getName() : string
+    {
+        return $this->name;
+    }
+    
+    public function getType() : string
+    {
+        return $this->type;
+    }
+    
+    public function getParams() : string
+    {
+        return $this->params;
+    }
+    
+    public function getMatchedString() : string
+    {
+        return $this->matchedString;
+    }
+}
