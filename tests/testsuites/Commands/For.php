85200198cce919f5e72cd2de353eac51e181d81c
Mistralys <s.mordziol@gmail.com>
2020-02-28 14:12:59 +0100

    Added the for command.

---
+++ tests/testsuites/Commands/For.php
@@ -0,0 +1,69 @@
+<?php
+
+use Mailcode\Mailcode;
+use Mailcode\Mailcode_Commands_Command;
+use Mailcode\Mailcode_Commands_Command_For;
+
+final class Mailcode_ForTests extends MailcodeTestCase
+{
+    public function test_validation()
+    {
+        $tests = array(
+            array(
+                'label' => 'Without parameters',
+                'string' => '{for}{end}',
+                'valid' => false,
+                'code' => Mailcode_Commands_Command::VALIDATION_MISSING_PARAMETERS
+            ),
+            array(
+                'label' => 'Empty parameters',
+                'string' => '{for: }{end}',
+                'valid' => false,
+                'code' => Mailcode_Commands_Command::VALIDATION_MISSING_PARAMETERS
+            ),
+            array(
+                'label' => 'With only a variable',
+                'string' => '{for: $FOO.BAR}{end}',
+                'valid' => false,
+                'code' => Mailcode_Commands_Command_For::VALIDATION_INVALID_FOR_STATEMENT
+            ),
+            array(
+                'label' => 'With invalid variable',
+                'string' => '{for: $5OOBAR in: $SOMEVAR.YO}{end}',
+                'valid' => false,
+                'code' => Mailcode_Commands_Command::VALIDATION_INVALID_PARAMS_STATEMENT
+            ),
+            array(
+                'label' => 'With missing container variable',
+                'string' => '{for: $FOO.BAR in: }{end}',
+                'valid' => false,
+                'code' => Mailcode_Commands_Command_For::VALIDATION_INVALID_FOR_STATEMENT
+            ),
+            array(
+                'label' => 'With  misspelled keyword',
+                'string' => '{for: $FOO in $BAR.FOO}{end}',
+                'valid' => false,
+                'code' => Mailcode_Commands_Command::VALIDATION_INVALID_PARAMS_STATEMENT
+            ),
+            array(
+                'label' => 'Valid statement',
+                'string' => '{for: $FOO.BAR in: $BAR.FOO}{end}',
+                'valid' => true,
+                'code' => 0
+            )
+        );
+        
+        foreach($tests as $test)
+        {
+            $collection = Mailcode::create()->parseString($test['string']);
+            
+            $this->assertSame($test['valid'], $collection->isValid(), $test['label']);
+            
+            if(!$test['valid'])
+            {
+                $errors = $collection->getErrors();
+                $this->assertSame($test['code'], $errors[0]->getCode(), $test['label']);
+            }
+        }
+    }
+}
