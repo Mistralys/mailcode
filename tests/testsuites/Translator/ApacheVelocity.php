4d3781050602c43d658c4f48b467b88b28940f68
Mistralys <s.mordziol@gmail.com>
2020-05-19 15:03:12 +0200

    Updated the date command syntax.

--- tests/testsuites/Translator/ApacheVelocity.php
+++ tests/testsuites/Translator/ApacheVelocity.php
@@ -27,17 +27,17 @@ final class Translator_ApacheVelocityTests extends MailcodeTestCase
             array(
                 'label' => 'Show date, default format',
                 'mailcode' => Mailcode_Factory::showDate('FOO.BAR'),
-                'expected' => '$date.format("yyyy/M/d", $FOO.BAR)'
+                'expected' => '$date.format("yyyy/M/d", $date.toDate("yyyy-MM-dd HH:mm:ss.SSS", $FOO.BAR))'
             ),
             array(
                 'label' => 'Show date, german format',
                 'mailcode' => Mailcode_Factory::showDate('FOO.BAR', 'd.m.Y H:i:s'),
-                'expected' => '$date.format("d.M.yyyy H:m:s", $FOO.BAR)'
+                'expected' => '$date.format("d.M.yyyy H:m:s", $date.toDate("yyyy-MM-dd HH:mm:ss.SSS", $FOO.BAR))'
             ),
             array(
                 'label' => 'Show date, short year format',
                 'mailcode' => Mailcode_Factory::showDate('FOO.BAR', 'd.m.y'),
-                'expected' => '$date.format("d.M.yy", $FOO.BAR)'
+                'expected' => '$date.format("d.M.yy", $date.toDate("yyyy-MM-dd HH:mm:ss.SSS", $FOO.BAR))'
             ),
             array(
                 'label' => 'Show snippet',
