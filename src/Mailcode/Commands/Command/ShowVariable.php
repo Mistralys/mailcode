22fc7da024fe622c48fa28b3d28053c9cca98bb6
Mistralys <s.mordziol@gmail.com>
2020-07-22 16:01:45 +0200

    Converted to use drop-in validation traits.

--- src/Mailcode/Commands/Command/ShowVariable.php
+++ src/Mailcode/Commands/Command/ShowVariable.php
@@ -20,16 +20,7 @@ namespace Mailcode;
  */
 class Mailcode_Commands_Command_ShowVariable extends Mailcode_Commands_Command implements Mailcode_Commands_Command_Type_Standalone
 {
-    const ERROR_NO_VARIABLE_AVAILABLE = 49301;
-    
-    const VALIDATION_VARIABLE_MISSING = 48401;
-    
-    const VALIDATION_VARIABLE_COUNT_MISMATCH = 48402;
-    
-   /**
-    * @var Mailcode_Variables_Variable|NULL
-    */
-    protected $variable;
+    use Mailcode_Traits_Commands_Validation_Variable;
     
     public function getName() : string
     {
@@ -61,65 +52,9 @@ class Mailcode_Commands_Command_ShowVariable extends Mailcode_Commands_Command i
         return false;
     }
     
-   /**
-    * Retrieves the variable to show.
-    * 
-    * NOTE: Only available once the command has been
-    * validated. Always use isValid() first.
-    * 
-    * @throws Mailcode_Exception
-    * @return Mailcode_Variables_Variable
-    */
-    public function getVariable() : Mailcode_Variables_Variable
-    {
-        $this->validate();
-        
-        if($this->variable instanceof Mailcode_Variables_Variable)
-        {
-            return $this->variable;
-        }
-        
-        throw new Mailcode_Exception(
-            'No variable available.',
-            'No variable is present at this time, or the validation failed.',
-            self::ERROR_NO_VARIABLE_AVAILABLE
-        );
-    }
-    
-   /**
-    * Retrieves the full name of the variable to show.
-    * 
-    * NOTE: Only available once the command has been
-    * validated. Always use isValid() first.
-    * 
-    * @throws Mailcode_Exception
-    * @return string
-    */
-    public function getVariableName() : string
-    {
-        return $this->getVariable()->getFullName();
-    }
-
-    protected function validateSyntax_require_variable() : void
-    {
-         $vars = $this->getVariables()->getGroupedByName();
-         $amount = count($vars);
-         
-         if($amount === 1)
-         {
-             $this->variable = array_pop($vars);
-             return;
-         }
-         
-         $this->validationResult->makeError(
-            t('Command has %1$s variables, %2$s expected.', $amount, 1),
-            self::VALIDATION_VARIABLE_COUNT_MISMATCH
-         );
-    }
-    
     protected function getValidations() : array
     {
-        return array('require_variable');
+        return array('variable');
     }
     
     public function generatesContent() : bool
