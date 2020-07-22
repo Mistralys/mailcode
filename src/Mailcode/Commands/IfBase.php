e2136172c8ce9a1e1b92bb7ce2681ed488af1cec
Mistralys <s.mordziol@gmail.com>
2020-07-22 11:52:54 +0200

    Moved constants to the common constants class.

--- src/Mailcode/Commands/IfBase.php
+++ src/Mailcode/Commands/IfBase.php
@@ -20,20 +20,6 @@ namespace Mailcode;
  */
 abstract class Mailcode_Commands_IfBase extends Mailcode_Commands_Command
 {
-    const VALIDATION_VARIABLE_MISSING = 49201;
-    const VALIDATION_CONTAINS_MISSING_SEARCH_TERM = 49202;
-    const VALIDATION_INVALID_KEYWORD = 49203;
-    const VALIDATION_OPERAND_MISSING = 49204;
-    const VALIDATION_OPERAND_NOT_COMPARISON = 49205;
-    const VALIDATION_INVALID_COMPARISON_TOKEN = 49206;
-    const VALIDATION_EXPECTED_KEYWORD = 49207;
-    const VALIDATION_NOTHING_AFTER_OPERAND = 49208;
-    
-    const ERROR_NO_VARIABLE_AVAILABLE = 52601;
-    const ERROR_NO_STRING_LITERAL_AVAILABLE = 52602;
-    const ERROR_NO_COMPARATOR_AVAILABLE = 52603;
-    const ERROR_NO_VALUE_AVAILABLE = 52604;
-    
     public function supportsType(): bool
     {
         return true;
