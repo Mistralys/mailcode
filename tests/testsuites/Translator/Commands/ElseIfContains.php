274b02871911f3c833438745ec4051cf6a7272d1
Mistralys <s.mordziol@gmail.com>
2020-07-23 17:40:41 +0200

    Backslash escaping tests; Several search terms.

--- tests/testsuites/Translator/Commands/ElseIfContains.php
+++ tests/testsuites/Translator/Commands/ElseIfContains.php
@@ -6,6 +6,14 @@ final class Translator_Velocity_ElseIfContainsTests extends VelocityTestCase
 {
     public function test_translateCommand()
     {
+        // NOTE: for readability purposes here in the tests,
+        // the expected amount of slashes are replaced by
+        // strings ([SLASH], [DBLSLASH]...).
+        // 
+        // Otherwise, since backslashes have to be escaped 
+        // in PHP in some cases, the examples here would be
+        // pretty cryptic.
+        
         $tests = array(
             array(
                 'label' => 'ElseIf contains',
@@ -15,12 +23,31 @@ final class Translator_Velocity_ElseIfContainsTests extends VelocityTestCase
             array(
                 'label' => 'ElseIf contains with slash',
                 'mailcode' => Mailcode_Factory::elseIfContains('FOO.BAR', 'Va\lue'),
-                'expected' => '#elseif($FOO.BAR.matches("(?s)Va\\\\lue"))'
+                'expected' => sprintf(
+                    '#elseif($FOO.BAR.matches("(?s)%s"))',
+                    'Va[FOURSLASH]lue'
+                )
             ),
             array(
                 'label' => 'ElseIf contains with special characters',
                 'mailcode' => Mailcode_Factory::elseIfContains('FOO.BAR', '6 + 4 * 3'),
-                'expected' => '#elseif($FOO.BAR.matches("(?s)6 \+ 4 \* 3"))'
+                'expected' => sprintf(
+                    '#elseif($FOO.BAR.matches("(?s)%s"))',
+                    '6 [DBLSLASH]+ 4 [DBLSLASH]* 3'
+                )
+            ),
+            array(
+                'label' => 'Several search terms',
+                'mailcode' => Mailcode_Factory::elseIfContainsAny('FOO.BAR', array('Foo', 'Bar')),
+                'expected' => '#elseif($FOO.BAR.matches("(?s)Foo") || $FOO.BAR.matches("(?s)Bar"))'
+            ),
+            array(
+                'label' => 'With quotes in search term',
+                'mailcode' => Mailcode_Factory::elseIfContains('FOO.BAR', 'Value, "weird" huh?'),
+                'expected' => sprintf(
+                    '#elseif($FOO.BAR.matches("(?s)%s"))',
+                    'Value, [SLASH]"weird[SLASH]" huh[DBLSLASH]?'
+                )
             )
         );
         
--- tests/testsuites/Translator/Commands/IfContains.php
+++ tests/testsuites/Translator/Commands/IfContains.php
@@ -6,6 +6,14 @@ final class Translator_Velocity_IfContainsTests extends VelocityTestCase
 {
     public function test_translateCommand()
     {
+        // NOTE: for readability purposes here in the tests,
+        // the expected amount of slashes are replaced by
+        // strings ([SLASH], [DBLSLASH]...).
+        //
+        // Otherwise, since backslashes have to be escaped
+        // in PHP in some cases, the examples here would be
+        // pretty cryptic.
+        
         $tests = array(
             array(
                 'label' => 'If contains',
@@ -15,12 +23,31 @@ final class Translator_Velocity_IfContainsTests extends VelocityTestCase
             array(
                 'label' => 'If contains with slash',
                 'mailcode' => Mailcode_Factory::ifContains('FOO.BAR', 'Va\lue'),
-                'expected' => '#if($FOO.BAR.matches("(?s)Va\\\\lue"))'
+                'expected' => sprintf(
+                    '#if($FOO.BAR.matches("(?s)%s"))',
+                    'Va[FOURSLASH]lue'
+                )
             ),
             array(
                 'label' => 'If contains with special characters',
                 'mailcode' => Mailcode_Factory::ifContains('FOO.BAR', '6 + 4 * 3'),
-                'expected' => '#if($FOO.BAR.matches("(?s)6 \+ 4 \* 3"))'
+                'expected' => sprintf(
+                    '#if($FOO.BAR.matches("(?s)%s"))',
+                    addslashes('6 [DBLSLASH]+ 4 [DBLSLASH]* 3')
+                )
+            ),
+            array(
+                'label' => 'Several search terms',
+                'mailcode' => Mailcode_Factory::ifContainsAny('FOO.BAR', array('Foo', 'Bar')),
+                'expected' => '#if($FOO.BAR.matches("(?s)Foo") || $FOO.BAR.matches("(?s)Bar"))'
+            ),
+            array(
+                'label' => 'If contains with slash',
+                'mailcode' => Mailcode_Factory::ifContains('FOO.BAR', 'Value, "quoted" yeah?'),
+                'expected' => sprintf(
+                    '#if($FOO.BAR.matches("(?s)%s"))',
+                    'Value, [SLASH]"quoted[SLASH]" yeah[DBLSLASH]?'
+                )
             )
         );
         
