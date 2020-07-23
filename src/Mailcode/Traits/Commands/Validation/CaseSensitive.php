046c231a5139bea3991a62aba839df6cbca5c699
Mistralys <s.mordziol@gmail.com>
2020-07-23 17:32:20 +0200

    Added getCaseToken().

--- src/Mailcode/Traits/Commands/Validation/CaseSensitive.php
+++ src/Mailcode/Traits/Commands/Validation/CaseSensitive.php
@@ -29,15 +29,35 @@ trait Mailcode_Traits_Commands_Validation_CaseSensitive
     */
     protected $caseInsensitive = false;
 
+   /**
+    * @var Mailcode_Parser_Statement_Tokenizer_Token_Keyword
+    */
+    protected $caseToken;
+    
     protected function validateSyntax_case_sensitive() : void
     {
         $val = $this->validator->createKeyword('insensitive');
         
         $this->caseInsensitive = $val->isValid();
+        
+        if($val->isValid())
+        {
+            $this->caseToken = $val->getToken();
+        }
     }
     
     public function isCaseInsensitive() : bool
     {
         return $this->caseInsensitive;
+    }
+    
+    public function getCaseToken() : ?Mailcode_Parser_Statement_Tokenizer_Token_Keyword
+    {
+        if($this->caseToken instanceof Mailcode_Parser_Statement_Tokenizer_Token_Keyword)
+        {
+            return $this->caseToken;
+        }
+        
+        return null;
     }
 }
