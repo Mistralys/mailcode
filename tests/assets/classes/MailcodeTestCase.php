6f8543278b6683ef3bb138d5b139fd5be7e991b0
Mistralys <s.mordziol@gmail.com>
2020-07-23 17:35:48 +0200

    Added runCollectionTests(), a unified method for command string parsing tests.

--- tests/assets/classes/MailcodeTestCase.php
+++ tests/assets/classes/MailcodeTestCase.php
@@ -1,7 +1,37 @@
 <?php
 
+use Mailcode\Mailcode;
 use PHPUnit\Framework\TestCase;
 
 abstract class MailcodeTestCase extends TestCase
 {
+    protected function runCollectionTests(array $tests) : void
+    {
+        foreach($tests as $test)
+        {
+            $collection = Mailcode::create()->parseString($test['string']);
+            
+            $label = $test['label'].PHP_EOL;
+            
+            if(!$collection->isValid())
+            {
+                $label .= "Messages:".PHP_EOL;
+                
+                foreach($collection->getErrors() as $error)
+                {
+                    $label .= $error->getMessage().PHP_EOL;
+                }
+            }
+            
+            $label .= 'Command:'.$test['string'];
+            
+            $this->assertSame($test['valid'], $collection->isValid(), $label);
+            
+            if(!$test['valid'])
+            {
+                $error = $collection->getFirstError();
+                $this->assertSame($test['code'], $error->getCode(), $label);
+            }
+        }
+    }
 }
