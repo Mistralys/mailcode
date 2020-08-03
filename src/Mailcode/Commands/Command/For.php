aaff033c472fa6664ab9d3624c01a37790c34566
Mistralys <s.mordziol@gmail.com>
2020-08-03 16:57:57 +0200

    Improved the validation.

--- src/Mailcode/Commands/Command/For.php
+++ src/Mailcode/Commands/Command/For.php
@@ -20,7 +20,27 @@ namespace Mailcode;
  */
 class Mailcode_Commands_Command_For extends Mailcode_Commands_Command implements Mailcode_Commands_Command_Type_Opening
 {
+    const ERROR_SOURCE_VARIABLE_NOT_AVAILABLE = 64101;
+    const ERROR_LOOP_VARIABLE_NOT_AVAILABLE = 64102;
+    
     const VALIDATION_INVALID_FOR_STATEMENT = 49701;
+    const VALIDATION_WRONG_KEYWORD = 49702;
+    const VALIDATION_VARIABLE_NAME_IS_THE_SAME = 49703;
+    
+   /**
+    * @var Mailcode_Parser_Statement_Tokenizer_Token_Variable|NULL
+    */
+    private $loopVar;
+    
+   /**
+    * @var Mailcode_Parser_Statement_Tokenizer_Token_Variable|NULL
+    */
+    private $sourceVar;
+    
+   /**
+    * @var Mailcode_Parser_Statement_Tokenizer_Token_Keyword|NULL
+    */
+    private $keyword;
     
     public function getName() : string
     {
@@ -55,7 +75,9 @@ class Mailcode_Commands_Command_For extends Mailcode_Commands_Command implements
     protected function getValidations() : array
     {
         return array(
-            'statement'
+            'statement',
+            'keyword',
+            'variable_names'
         );
     }
     
@@ -64,22 +86,76 @@ class Mailcode_Commands_Command_For extends Mailcode_Commands_Command implements
         return false;
     }
     
+    public function getSourceVariable() : Mailcode_Variables_Variable
+    {
+        if(isset($this->sourceVar))
+        {
+            return $this->sourceVar->getVariable();
+        }
+        
+        throw new Mailcode_Exception(
+            'No source variable available',
+            null,
+            self::ERROR_SOURCE_VARIABLE_NOT_AVAILABLE
+        );
+    }
+    
+    public function getLoopVariable() : Mailcode_Variables_Variable
+    {
+        if(isset($this->loopVar))
+        {
+            return $this->loopVar->getVariable();
+        }
+        
+        throw new Mailcode_Exception(
+            'No loop variable available',
+            null,
+            self::ERROR_LOOP_VARIABLE_NOT_AVAILABLE
+        );
+    }
+    
     protected function validateSyntax_statement() : void
     {
         $info = $this->params->getInfo();
         
-        $variable = $info->getVariableByIndex(0);
-        $keyword = $info->getKeywordByIndex(1);
-        $container = $info->getVariableByIndex(2);
+        $this->loopVar = $info->getVariableByIndex(0);
+        $this->keyword = $info->getKeywordByIndex(1);
+        $this->sourceVar = $info->getVariableByIndex(2);
         
-        if($variable && $keyword && $container && $keyword->isForIn())
+        if(!$this->loopVar || !$this->keyword || !$this->sourceVar)
+        {
+            $this->validationResult->makeError(
+                t('Not a valid for loop.').' '.t('Is the %1$s keyword missing?', 'in:'),
+                self::VALIDATION_INVALID_FOR_STATEMENT
+            );
+            
+            return;
+        }
+    }
+    
+    protected function validateSyntax_keyword() : void
+    {
+        if($this->keyword->isForIn())
+        {
+            return;
+        }
+         
+        $this->validationResult->makeError(
+            t('The %1$s keyword cannot be used in this command.', $this->keyword->getKeyword()),
+            self::VALIDATION_WRONG_KEYWORD
+        );
+    }
+    
+    protected function validateSyntax_variable_names() : void
+    {
+        if($this->sourceVar->getVariable()->getFullName() !== $this->loopVar->getVariable()->getFullName())
         {
             return;
         }
         
         $this->validationResult->makeError(
-            t('Not a valid for loop.').' '.t('Is the %1$s keyword missing?', 'in:'),
-            self::VALIDATION_INVALID_FOR_STATEMENT
+            t('The source and loop variables have the same name.'),
+            self::VALIDATION_VARIABLE_NAME_IS_THE_SAME
         );
     }
 }
