7097d5b3a82d5d98fd8d9f31293db4f16bdbec0e
Mistralys <s.mordziol@gmail.com>
2020-09-01 18:56:43 +0200

    Added base classes for formatters that replace or just format the code.

---
+++ src/Mailcode/Parser/Safeguard/Formatter/FormatType.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * File containing the {@see Mailcode_Parser_Safeguard_Formatter_Type_Highlighting} class.
+ *
+ * @package Mailcode
+ * @subpackage Parser
+ * @see Mailcode_Parser_Safeguard_Formatter_Type_Highlighting
+ */
+
+declare(strict_types=1);
+
+namespace Mailcode;
+
+/**
+ * Abstract base class for replacer formatters that replace the placeholders.
+ *
+ * @package Mailcode
+ * @subpackage Parser
+ * @author Sebastian Mordziol <s.mordziol@mistralys.eu>
+ */
+abstract class Mailcode_Parser_Safeguard_Formatter_FormatType extends Mailcode_Parser_Safeguard_Formatter
+{
+    public function getPriority() : int
+    {
+        return PHP_INT_MAX;
+    }
+
+   /**
+    * Formats the specified string according to the formatter.
+    * Retrieve the updated string from the string container used
+    * to create the formatter, or use `getSubjectString()`.
+    */
+    public function format() : void
+    {
+        $locations = $this->resolveLocations();
+        
+        foreach($locations as $location)
+        {
+            $location->format();
+        }
+    }
+}
---
+++ src/Mailcode/Parser/Safeguard/Formatter/ReplacerType.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * File containing the {@see Mailcode_Parser_Safeguard_Formatter_Type_Highlighting} class.
+ *
+ * @package Mailcode
+ * @subpackage Parser
+ * @see Mailcode_Parser_Safeguard_Formatter_Type_Highlighting
+ */
+
+declare(strict_types=1);
+
+namespace Mailcode;
+
+/**
+ * Abstract base class for replacer formatters that replace the placeholders.
+ *
+ * @package Mailcode
+ * @subpackage Parser
+ * @author Sebastian Mordziol <s.mordziol@mistralys.eu>
+ */
+abstract class Mailcode_Parser_Safeguard_Formatter_ReplacerType extends Mailcode_Parser_Safeguard_Formatter
+{
+    public function getPriority() : int
+    {
+        return PHP_INT_MAX * -1;
+    }
+    
+    public function replace() : void
+    {
+        $locations = $this->resolveLocations();
+        
+        foreach($locations as $location)
+        {
+            $location->replaceWith($this->resolveReplacement($location));
+        }
+    }
+    
+   /**
+    * Resolves the string with which this location needs to be
+    * replaced.
+    * 
+    * @param Mailcode_Parser_Safeguard_Formatter_Location $location
+    * @return string
+    */
+    private function resolveReplacement(Mailcode_Parser_Safeguard_Formatter_Location $location) : string
+    {
+        if($location->requiresAdjustment())
+        {
+            return $this->getReplaceString($location);
+        }
+
+        return $location->getPlaceholder()->getNormalizedText();
+    }
+    
+    abstract public function getReplaceString(Mailcode_Parser_Safeguard_Formatter_Location $location) : string;
+}
+    
