84ddb69451822387d0d435c86aa41042557c7cd1
Mistralys <s.mordziol@gmail.com>
2020-09-01 19:01:19 +0200

    Dummy test class used to run some individual tests in isolation as needed.

---
+++ tests/testsuites/Isolation/IsolatedTests.php
@@ -0,0 +1,13 @@
+<?php
+
+final class Isolation_IsolatedTests extends MailcodeTestCase
+{
+    public function test_isolated()
+    {
+        $this->addToAssertionCount(1);
+    }
+    
+    // -------------------------------------------------------
+    // USED IN CASE IT IS NECESSARY TO RUN INDIVIDUAL TESTS.
+    // -------------------------------------------------------
+}
