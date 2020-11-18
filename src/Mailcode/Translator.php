38fe1b9030c943dd602c46cf55bd7243ed291af3
Mistralys <s.mordziol@gmail.com>
2020-11-18 13:27:58 +0100

    Added createSyntax(), getSyntaxes(), getSyntaxNames(), syntaxExists().

--- src/Mailcode/Translator.php
+++ src/Mailcode/Translator.php
@@ -11,6 +11,8 @@ declare(strict_types=1);
 
 namespace Mailcode;
 
+use AppUtils\FileHelper;
+
 /**
  * Used to translate mailcode syntax to other syntaxes.
  *
@@ -20,10 +22,73 @@ namespace Mailcode;
  */
 class Mailcode_Translator
 {
-    public function createSyntax(string $syntaxID) : Mailcode_Translator_Syntax
+    const ERROR_INVALID_SYNTAX_NAME = 73001;
+
+    /**
+     * Creates an instance of the specified syntax.
+     *
+     * @param string $name The name of the syntax, e.g. "ApacheVelocity"
+     * @return Mailcode_Translator_Syntax
+     */
+    public function createSyntax(string $name) : Mailcode_Translator_Syntax
     {
-        $class = '\Mailcode\Mailcode_Translator_Syntax';
-        
-        return new $class($syntaxID);
+        if($this->syntaxExists($name))
+        {
+            return new Mailcode_Translator_Syntax($name);
+        }
+
+        throw new Mailcode_Exception(
+            'Invalid translation syntax',
+            sprintf(
+                'The syntax [%s] does not exist. Possible values are: [%s].',
+                $name,
+                implode(', ', $this->getSyntaxNames())
+            ),
+            self::ERROR_INVALID_SYNTAX_NAME
+        );
+    }
+
+    /**
+     * Retrieves an instance for each syntax available
+     * in the system.
+     *
+     * @return Mailcode_Translator_Syntax[]
+     */
+    public function getSyntaxes() : array
+    {
+        $names = $this->getSyntaxNames();
+        $result = array();
+
+        foreach($names as $name)
+        {
+            $result[] = $this->createSyntax($name);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Retrieves a list of the names of all syntaxes supported
+     * by the system.
+     *
+     * @return string[]
+     */
+    public function getSyntaxNames() : array
+    {
+        return FileHelper::createFileFinder(__DIR__.'/Translator/Syntax')
+            ->getPHPClassNames();
+    }
+
+    /**
+     * Checks whether the specified syntax exists.
+     *
+     * @param string $name The syntax name, e.g. "ApacheVelocity"
+     * @return bool
+     */
+    public function syntaxExists(string $name) : bool
+    {
+        $names = $this->getSyntaxNames();
+
+        return in_array($name, $names);
     }
 }
