f362d1b657b2069d1006569180a5f2d8e3004806
Mistralys <s.mordziol@gmail.com>
2021-02-04 12:23:39 +0100

    Added `debugVariablesCollection()`.

--- tests/assets/classes/MailcodeTestCase.php
+++ tests/assets/classes/MailcodeTestCase.php
@@ -2,6 +2,7 @@
 
 use Mailcode\Mailcode;
 use Mailcode\Mailcode_Exception;
+use Mailcode\Mailcode_Variables_Collection;
 use PHPUnit\Framework\TestCase;
 
 abstract class MailcodeTestCase extends TestCase
@@ -50,5 +51,47 @@ abstract class MailcodeTestCase extends TestCase
                 }
             }
         }
+    }
+
+    /**
+     * Dumps a list of all variables in the collection as a string
+     * which includes the names in order of appearance, grouped by
+     * name and grouped by unique name.
+     *
+     * @param Mailcode_Variables_Collection $collection
+     * @return string
+     * @throws Mailcode_Exception
+     */
+    protected function debugVariablesCollection(Mailcode_Variables_Collection $collection) : string
+    {
+        $list = $collection->getAll();
+
+        $distinct = array();
+        $names = array();
+        $uniqueNames = array();
+
+        foreach($list as $variable)
+        {
+            $distinct[] = $variable->getFullName();
+            $names[] = $variable->getFullName();
+            $uniqueNames[] = $variable->getUniqueName();
+        }
+
+        $names = array_unique($names);
+        $uniqueNames = array_unique($uniqueNames);
+
+        sort($names);
+        sort($uniqueNames);
+
+        return
+            PHP_EOL.'----------------------------------------------------------'.PHP_EOL.
+            'VARIABLES DUMP'.PHP_EOL.
+            PHP_EOL.'----------------------------------------------------------'.PHP_EOL.
+            'In order of appearance ('.count($distinct).'):'.PHP_EOL.
+            '  -'.implode(PHP_EOL.'  -', $distinct).PHP_EOL.
+            'Grouped by name ('.count($names).'):'.PHP_EOL.
+            '  -'.implode(PHP_EOL.'  -', $names).PHP_EOL.
+            'Unique instances by command ('.count($uniqueNames).'):'.PHP_EOL.
+            '  -'.implode(PHP_EOL.'  -', $uniqueNames).PHP_EOL.PHP_EOL;
     }
 }
