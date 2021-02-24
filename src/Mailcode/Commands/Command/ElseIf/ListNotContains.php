8fb694c1835f846dea690b8fe186945a961df599
Mistralys <s.mordziol@gmail.com>
2021-02-24 12:37:17 +0100

    Added missing traits / interfaces.

--- src/Mailcode/Commands/Command/ElseIf/ListNotContains.php
+++ src/Mailcode/Commands/Command/ElseIf/ListNotContains.php
@@ -20,6 +20,29 @@ namespace Mailcode;
  * @subpackage Commands
  * @author Sebastian Mordziol <s.mordziol@mistralys.eu>
  */
-class Mailcode_Commands_Command_ElseIf_ListNotContains extends Mailcode_Commands_Command_ElseIf_NotContains
+class Mailcode_Commands_Command_ElseIf_ListNotContains
+    extends
+        Mailcode_Commands_Command_ElseIf_NotContains
+    implements
+        Mailcode_Interfaces_Commands_ListVariables,
+        Mailcode_Interfaces_Commands_ListPropertyVariable,
+        Mailcode_Interfaces_Commands_RegexEnabled
 {
+    use Mailcode_Traits_Commands_ListVariables;
+    use Mailcode_Traits_Commands_Validation_ListPropertyVariable;
+    use Mailcode_Traits_Commands_Validation_RegexEnabled;
+
+    protected function getValidations(): array
+    {
+        $validations = parent::getValidations();
+        $validations[] = 'list_property_variable';
+        $validations[] = 'regex_enabled';
+
+        return $validations;
+    }
+
+    protected function _collectListVariables(Mailcode_Variables_Collection_Regular $collection): void
+    {
+        $collection->add($this->getListVariable());
+    }
 }
--- src/Mailcode/Commands/Command/If/ListContains.php
+++ src/Mailcode/Commands/Command/If/ListContains.php
@@ -20,15 +20,24 @@ namespace Mailcode;
  * @subpackage Commands
  * @author Sebastian Mordziol <s.mordziol@mistralys.eu>
  */
-class Mailcode_Commands_Command_If_ListContains extends Mailcode_Commands_Command_If_Contains implements Mailcode_Interfaces_Commands_ListVariables, Mailcode_Interfaces_Commands_ListPropertyVariable
+class Mailcode_Commands_Command_If_ListContains
+    extends
+        Mailcode_Commands_Command_If_Contains
+    implements
+        Mailcode_Interfaces_Commands_ListVariables,
+        Mailcode_Interfaces_Commands_ListPropertyVariable,
+        Mailcode_Interfaces_Commands_RegexEnabled
 {
+
     use Mailcode_Traits_Commands_ListVariables;
     use Mailcode_Traits_Commands_Validation_ListPropertyVariable;
+    use Mailcode_Traits_Commands_Validation_RegexEnabled;
 
     protected function getValidations(): array
     {
         $validations = parent::getValidations();
         $validations[] = 'list_property_variable';
+        $validations[] = 'regex_enabled';
 
         return $validations;
     }
