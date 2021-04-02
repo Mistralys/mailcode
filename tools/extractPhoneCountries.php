3dfa5c2de87c523a2cabc942490f90ccb304cc3d
Mistralys <s.mordziol@gmail.com>
2021-04-02 13:45:04 +0200

    Updated layout somewhat, added error check when loading the XML.

--- tools/extractPhoneCountries.php
+++ tools/extractPhoneCountries.php
@@ -11,8 +11,9 @@
 declare(strict_types=1);
 
 use AppUtils\FileHelper;
+use AppUtils\FileHelper_Exception;
 
-require_once '../vendor/autoload.php';
+require_once 'prepend.php';
 
 $xmlURL = 'https://raw.githubusercontent.com/google/libphonenumber/master/resources/PhoneNumberMetadata.xml';
 
@@ -21,8 +22,15 @@ $cacheExpiry = 60 * 60; // 1 hour
 
 if(!file_exists($cacheFile) || (filemtime($cacheFile) + $cacheExpiry) < time())
 {
-    $xml = FileHelper::downloadFile($xmlURL);
-    FileHelper::saveFile($cacheFile, $xml);
+    try
+    {
+        $xml = FileHelper::downloadFile($xmlURL);
+        FileHelper::saveFile($cacheFile, $xml);
+    }
+    catch (FileHelper_Exception $e)
+    {
+        die('Unable to download the current libphonenumber XML from <a href="'.$xmlURL.'">'.$xmlURL.'</a>');
+    }
 }
 else
 {
@@ -50,30 +58,29 @@ $code = 'array('.PHP_EOL.implode(",".PHP_EOL, $lines).PHP_EOL.');';
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Mailcode - Phone format countries</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
         <style>
             BODY{
-                font-family: sans-serif;
-                padding:2em;
-            }
-            TEXTAREA{
-                width: 100%;
+                padding:2em 0;
             }
         </style>
     </head>
     <body>
-        <h1>Phone number countries extraction</h1>
-        <p>
-            This downloads Google's official LibPhoneNumber countries XML,
-            and extracts a list of countries from it, for use in the <code>{showphone}</code>
-            command class, to validate the source country phone format parameter.
-        </p>
-        <p>
-            Target class: <code>Mailcode_Commands_Command_ShowPhone</code>
-        </p>
-        <p>
-            It is meant to be pasted into the class' <code>$supportedCountries</code>
-            property.
-        </p>
-        <textarea rows="20"><?php echo $code ?></textarea>
+        <div class="container">
+            <h1>Phone number countries extraction</h1>
+            <p>
+                This downloads Google's official LibPhoneNumber countries XML,
+                and extracts a list of countries from it, for use in the <code>{showphone}</code>
+                command class, to validate the source country phone format parameter.
+            </p>
+            <p>
+                Target class: <code>Mailcode_Commands_Command_ShowPhone</code>
+            </p>
+            <p>
+                It is meant to be pasted into the class' <code>$supportedCountries</code>
+                property.
+            </p>
+            <textarea rows="20" class="form-control"><?php echo $code ?></textarea>
+        </div>
     </body>
 </html>
