41cb55d63dba3d6c3c0bcec6b1eaebe6f6bb48f4
Mistralys <s.mordziol@gmail.com>
2021-04-02 13:40:58 +0200

    Added a tool to translate texts with Mailcode to a target syntax.

---
+++ tools/translator.php
@@ -0,0 +1,168 @@
+<?php
+/**
+ * Utility script that can translate a text with Mailcode commands
+ * to any of the available translation syntaxes. Meant to be opened
+ * in a browser.
+ *
+ * @package Mailcode
+ * @subpackage Tools
+ * @author Sebastian Mordziol <s.mordziol@mistralys.eu>
+ */
+
+declare(strict_types=1);
+
+use AppUtils\Request;
+use Mailcode\Mailcode;
+use Mailcode\Mailcode_Exception;
+use Mailcode\Mailcode_Translator_Exception;
+use function \AppLocalize\pt;
+use function \AppLocalize\pts;
+
+require_once 'prepend.php';
+
+$request = new Request();
+$mailcode = Mailcode::create();
+$translator = $mailcode->createTranslator();
+$syntaxes = $translator->getSyntaxes();
+
+$commandsText = '';
+$translated = '';
+$error = null;
+$activeSyntax = $syntaxes[0]->getTypeID();
+
+if($request->getBool('translate'))
+{
+    $commandsText = $request->getParam('mailcode');
+    $activeSyntax = (string)$request->registerParam('syntax')
+        ->setEnum($translator->getSyntaxNames())
+        ->get($activeSyntax);
+
+    try
+    {
+        $translated = translateMailcode($commandsText, $activeSyntax);
+    }
+    catch (Mailcode_Exception $e)
+    {
+        $translated = '';
+        $error = $e->getMessage();
+
+        $collection = $e->getCollection();
+        if($collection)
+        {
+            $first = $collection->getFirstError();
+            $error = $first->getMessage();
+            $matched = $first->getMatchedText();
+            if(!empty($matched)) {
+                $error .= '<br>'.t('In command:').' <code>'.$matched.'</code>';
+            }
+        }
+    }
+}
+
+/**
+ * Translates the specified text to Mailcode.
+ *
+ * @param string $subject
+ * @param string $syntax
+ * @return string
+ * @throws Mailcode_Exception
+ * @throws Mailcode_Translator_Exception
+ */
+function translateMailcode(string $subject, string $syntax) : string
+{
+    $mailcode = Mailcode::create();
+    $translator = $mailcode->createTranslator()->createSyntax($syntax);
+
+    $safeguard = $mailcode->createSafeguard($subject);
+    return $translator->translateSafeguard($safeguard);
+}
+
+?><!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Mailcode commands translator</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <style>
+        <?php echo $mailcode->createStyler()->getCSS() ?>
+        BODY{
+            padding: 2rem 0;
+        }
+        TEXTAREA{
+            width: 100%;
+        }
+
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1><?php pt('Commands translation') ?></h1>
+        <p>
+            <?php
+                pts('Enter the text that contains the mailcode commands to convert, and choose the output language to convert them to.');
+                pts('Can be any kind of text, including HTML/XML.');
+            ?>
+        </p>
+        <p class="text-warning">
+            <strong><?php pt('Note:') ?></strong>
+            <?php pt('The Mailcode commands must be valid.') ?>
+        </p>
+        <form method="post">
+            <p>
+                <textarea class="form-control" name="mailcode" rows="10"><?php echo htmlspecialchars($commandsText) ?></textarea>
+            </p>
+            <p>
+                <select name="syntax" class="form-control">
+                    <?php
+                        foreach ($syntaxes as $syntax)
+                        {
+                            $selected = '';
+                            $typeID = $syntax->getTypeID();
+
+                            if($typeID === $activeSyntax) {
+                                $selected = ' selected';
+                            }
+
+                            ?>
+                                <option value="<?php echo $syntax->getTypeID() ?>>" <?php echo $selected ?>>
+                                    <?php echo $syntax->getTypeID() ?>
+                                </option>
+                            <?php
+                        }
+                    ?>
+                </select>
+            </p>
+            <button type="submit" name="translate" value="yes" class="btn btn-primary">
+                <?php pt('Translate commands') ?>
+            </button>
+        </form>
+        <p></p><br>
+        <h2><?php pt('Translated commands') ?></h2>
+        <?php
+            if(empty($commandsText))
+            {
+                ?>
+                    <div class="alert alert-info">
+                        <?php pt('No commands specified.') ?>
+                    </div>
+                <?php
+            }
+            else if($error)
+            {
+                ?>
+                    <div class="alert alert-danger">
+                        <strong><?php pt('Error parsing commands:') ?></strong>
+                        <?php echo $error ?>
+                    </div>
+                <?php
+            }
+            else
+            {
+                ?>
+                    <textarea class="form-control" rows="10"><?php echo htmlspecialchars($translated) ?></textarea>
+                <?php
+            }
+        ?>
+    </div>
+</body>
+</html>
