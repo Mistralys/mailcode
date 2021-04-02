bc6a27be9cae52246c3a007be811af3922977d98
Mistralys <s.mordziol@gmail.com>
2021-04-02 17:00:04 +0200

    Revamped to use the new `numbers.json` data file.

--- src/Mailcode/Commands/Command/ShowPhone.php
+++ src/Mailcode/Commands/Command/ShowPhone.php
@@ -11,6 +11,9 @@ declare(strict_types=1);
 
 namespace Mailcode;
 
+use AppUtils\FileHelper;
+use AppUtils\FileHelper_Exception;
+
 /**
  * Mailcode command: show a phone number variable value, in E164 format.
  *
@@ -35,186 +38,14 @@ class Mailcode_Commands_Command_ShowPhone extends Mailcode_Commands_ShowBase
      *
      * @var array<string,string>
      *
-     * @see https://github.com/google/libphonenumber/tree/master/resources/geocoding
      * @see /tools/extractPhoneCountries.php
      */
-    protected static $supportedCountries = array(
-        'AD' => 'Andorra',
-        'AF' => 'Afghanistan',
-        'AI' => 'Anguilla',
-        'AL' => 'Albania',
-        'AM' => 'Armenia',
-        'AO' => 'Angola',
-        'AR' => 'Argentina',
-        'AT' => 'Austria',
-        'AU' => 'Australia',
-        'AW' => 'Aruba',
-        'AZ' => 'Azerbaijan',
-        'BB' => 'Barbados',
-        'BD' => 'Bangladesh',
-        'BE' => 'Belgium',
-        'BG' => 'Bulgaria',
-        'BH' => 'Bahrain',
-        'BI' => 'Burundi',
-        'BJ' => 'Benin',
-        'BM' => 'Bermuda',
-        'BN' => 'Brunei',
-        'BO' => 'Bolivia',
-        'BR' => 'Brazil',
-        'BS' => 'Bahamas',
-        'BT' => 'Bhutan',
-        'BW' => 'Botswana',
-        'BY' => 'Belarus',
-        'BZ' => 'Belize',
-        'CA' => 'Canada',
-        'CH' => 'Switzerland',
-        'CL' => 'Chile',
-        'CM' => 'Cameroon',
-        'CN' => 'China',
-        'CO' => 'Colombia',
-        'CU' => 'Cuba',
-        'CY' => 'Cyprus',
-        'CZ' => 'Czechia',
-        'DE' => 'Germany',
-        'DJ' => 'Djibouti',
-        'DK' => 'Denmark',
-        'DM' => 'Dominica',
-        'DZ' => 'Algeria',
-        'EC' => 'Ecuador',
-        'EE' => 'Estonia',
-        'EG' => 'Egypt',
-        'ER' => 'Eritrea',
-        'ES' => 'Spain',
-        'ET' => 'Ethiopia',
-        'FI' => 'Finland',
-        'FJ' => 'Fiji',
-        'FM' => 'Micronesia',
-        'FR' => 'France',
-        'GA' => 'Gabon',
-        'GD' => 'Grenada',
-        'GE' => 'Georgia',
-        'GG' => 'Guernsey',
-        'GH' => 'Ghana',
-        'GI' => 'Gibraltar',
-        'GL' => 'Greenland',
-        'GM' => 'Gambia',
-        'GN' => 'Guinea',
-        'GP' => 'Guadeloupe',
-        'GR' => 'Greece',
-        'GT' => 'Guatemala',
-        'GU' => 'Guam',
-        'GY' => 'Guyana',
-        'HN' => 'Honduras',
-        'HR' => 'Croatia',
-        'HT' => 'Haiti',
-        'HU' => 'Hungary',
-        'ID' => 'Indonesia',
-        'IE' => 'Ireland',
-        'IL' => 'Israel',
-        'IN' => 'India',
-        'IQ' => 'Iraq',
-        'IR' => 'Iran',
-        'IS' => 'Iceland',
-        'IT' => 'Italy',
-        'JE' => 'Jersey',
-        'JM' => 'Jamaica',
-        'JO' => 'Jordan',
-        'JP' => 'Japan',
-        'KE' => 'Kenya',
-        'KG' => 'Kyrgyzstan',
-        'KH' => 'Cambodia',
-        'KI' => 'Kiribati',
-        'KM' => 'Comoros',
-        'KW' => 'Kuwait',
-        'KZ' => 'Kazakhstan',
-        'LA' => 'Laos',
-        'LB' => 'Lebanon',
-        'LI' => 'Liechtenstein',
-        'LR' => 'Liberia',
-        'LS' => 'Lesotho',
-        'LT' => 'Lithuania',
-        'LU' => 'Luxembourg',
-        'LV' => 'Latvia',
-        'LY' => 'Libya',
-        'MA' => 'Morocco',
-        'MC' => 'Monaco',
-        'MD' => 'Moldova',
-        'ME' => 'Montenegro',
-        'MG' => 'Madagascar',
-        'ML' => 'Mali',
-        'MN' => 'Mongolia',
-        'MO' => 'Macao',
-        'MQ' => 'Martinique',
-        'MR' => 'Mauritania',
-        'MS' => 'Montserrat',
-        'MT' => 'Malta',
-        'MU' => 'Mauritius',
-        'MV' => 'Maldives',
-        'MW' => 'Malawi',
-        'MX' => 'Mexico',
-        'MY' => 'Malaysia',
-        'MZ' => 'Mozambique',
-        'NA' => 'Namibia',
-        'NE' => 'Niger',
-        'NG' => 'Nigeria',
-        'NI' => 'Nicaragua',
-        'NL' => 'Netherlands',
-        'NO' => 'Norway',
-        'NP' => 'Nepal',
-        'NR' => 'Nauru',
-        'NU' => 'Niue',
-        'OM' => 'Oman',
-        'PA' => 'Panama',
-        'PE' => 'Peru',
-        'PH' => 'Philippines',
-        'PK' => 'Pakistan',
-        'PL' => 'Poland',
-        'PS' => 'Palestine',
-        'PT' => 'Portugal',
-        'PW' => 'Palau',
-        'PY' => 'Paraguay',
-        'QA' => 'Qatar',
-        'RO' => 'Romania',
-        'RS' => 'Serbia',
-        'RU' => 'Russia',
-        'RW' => 'Rwanda',
-        'SC' => 'Seychelles',
-        'SD' => 'Sudan',
-        'SE' => 'Sweden',
-        'SG' => 'Singapore',
-        'SI' => 'Slovenia',
-        'SK' => 'Slovakia',
-        'SN' => 'Senegal',
-        'SO' => 'Somalia',
-        'SR' => 'Suriname',
-        'SY' => 'Syria',
-        'SZ' => 'Eswatini',
-        'TD' => 'Chad',
-        'TG' => 'Togo',
-        'TH' => 'Thailand',
-        'TJ' => 'Tajikistan',
-        'TK' => 'Tokelau',
-        'TM' => 'Turkmenistan',
-        'TN' => 'Tunisia',
-        'TO' => 'Tonga',
-        'TR' => 'Turkey',
-        'TV' => 'Tuvalu',
-        'TW' => 'Taiwan',
-        'TZ' => 'Tanzania',
-        'UA' => 'Ukraine',
-        'UG' => 'Uganda',
-        'UY' => 'Uruguay',
-        'UZ' => 'Uzbekistan',
-        'VE' => 'Venezuela',
-        'VN' => 'Vietnam',
-        'VU' => 'Vanuatu',
-        'WS' => 'Samoa',
-        'XK' => 'Kosovo',
-        'YE' => 'Yemen',
-        'YT' => 'Mayotte',
-        'ZM' => 'Zambia',
-        'ZW' => 'Zimbabwe'
-    );
+    protected static $supportedCountries = array();
+
+    /**
+     * @var bool
+     */
+    protected static $countriesLoaded = false;
 
     public function getName() : string
     {
@@ -255,10 +86,14 @@ class Mailcode_Commands_Command_ShowPhone extends Mailcode_Commands_ShowBase
     /**
      * Validates the specified country code to ensure it is one of the
      * supported countries.
+     *
+     * @throws FileHelper_Exception
      */
     protected function validateSyntax_country_code(): void
     {
-        if(isset(self::$supportedCountries[$this->sourceFormat])) {
+        $countries = self::getSupportedCountries();
+
+        if(isset($countries[$this->sourceFormat])) {
             return;
         }
 
@@ -272,10 +107,29 @@ class Mailcode_Commands_Command_ShowPhone extends Mailcode_Commands_ShowBase
      * Retrieves the list of countries supported for phone number conversions,
      * as an associative array with uppercase country code => country name pairs.
      *
-     * @return array<string,string>
+     * @return Mailcode_Commands_Command_ShowPhone_Number[]
+     * @throws FileHelper_Exception
      */
     public static function getSupportedCountries() : array
     {
+        if(self::$countriesLoaded) {
+            return self::$supportedCountries;
+        }
+
+        self::$countriesLoaded = true;
+
+        $data = FileHelper::parseJSONFile(__DIR__.'/ShowPhone/numbers.json');
+
+        foreach($data as $code => $def)
+        {
+            self::$supportedCountries[$code] = new Mailcode_Commands_Command_ShowPhone_Number(
+                $code,
+                strval($def['label']),
+                strval($def['local']),
+                strval($def['international'])
+            );
+        }
+
         return self::$supportedCountries;
     }
 
