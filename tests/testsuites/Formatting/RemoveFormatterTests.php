d114ac266093426d3f84baeb273dce24f38c7868
Mistralys <s.mordziol@gmail.com>
2022-02-22 18:14:16 +0100

    Added tests.

---
+++ tests/testsuites/Formatting/RemoveFormatterTests.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace testsuites\Formatting;
+
+use Mailcode\Mailcode;
+use MailcodeTestCase;
+
+final class RemoveFormatterTests extends MailcodeTestCase
+{
+    /**
+     * When removing commands, whitespace is preserved
+     * even when using logic commands.
+     */
+    public function test_leftoverText() : void
+    {
+        $subject = <<<'EOT'
+{if variable: $FOO == "value"}
+Variable here: {showvar: $BAR}
+{end}
+End of string: {showvar: $FOOBAR}.
+EOT;
+
+        $expected = <<<'EOT'
+
+Variable here: 
+
+End of string: .
+EOT;
+
+        $safeguard = Mailcode::create()->createSafeguard($subject);
+        
+        $this->assertSafeguardValid($safeguard);
+        
+        $safe = $safeguard->makeSafe();
+            
+        $formatting = $safeguard->createFormatting($safe);
+        $formatting->makePartial();
+        $formatting->replaceWithRemovedCommands();
+
+        $result = $formatting->toString();
+
+        $this->assertSame($expected, $result);
+    }
+
+    /**
+     * Code commands must be entirely replaced by an empty string,
+     * regardless of their content.
+     */
+    public function test_codeCommands() : void
+    {
+        $subject = <<<'EOT'
+{code: "ApacheVelocity"}
+    Some content of the command.
+    {showvar: $SUB.COMMAND}
+{end}
+EOT;
+
+        $safeguard = Mailcode::create()->createSafeguard($subject);
+
+        $this->assertSafeguardValid($safeguard);
+
+        $safe = $safeguard->makeSafe();
+
+        $formatting = $safeguard->createFormatting($safe);
+        $formatting->makePartial();
+        $formatting->replaceWithRemovedCommands();
+
+        $result = $formatting->toString();
+
+        $this->assertSame('', $result);
+    }
+}
