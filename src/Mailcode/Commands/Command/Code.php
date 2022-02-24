10a2b609b570ffe3e0c5b7fde8203239cbcadb12
Mistralys <s.mordziol@gmail.com>
2022-02-24 08:03:45 +0100

    Now handling the syntaxes separately.

--- src/Mailcode/Commands/Command/Code.php
+++ src/Mailcode/Commands/Command/Code.php
@@ -21,11 +21,10 @@ namespace Mailcode;
 class Mailcode_Commands_Command_Code
     extends Mailcode_Commands_Command
     implements
-        Mailcode_Commands_Command_Type_Opening,
+        Mailcode_Commands_Command_Type_Standalone,
         Mailcode_Interfaces_Commands_ProtectedContent
 {
     use Mailcode_Traits_Commands_ProtectedContent;
-    use Mailcode_Traits_Commands_Type_Opening;
 
     public const ERROR_LANG_TOKEN_MISSING = 73101;
 
@@ -35,7 +34,7 @@ class Mailcode_Commands_Command_Code
     /**
      * @var Mailcode_Parser_Statement_Tokenizer_Token_StringLiteral|NULL
      */
-    private $langToken = null;
+    private ?Mailcode_Parser_Statement_Tokenizer_Token_StringLiteral $langToken = null;
 
     public function getName() : string
     {
@@ -75,6 +74,7 @@ class Mailcode_Commands_Command_Code
     protected function getValidations() : array
     {
         return array(
+            Mailcode_Interfaces_Commands_ProtectedContent::VALIDATION_NAME_CONTENT_ID,
             'token',
             'language'
         );
@@ -85,12 +85,49 @@ class Mailcode_Commands_Command_Code
         return true;
     }
 
+    // region: Supported syntaxes
+
+    public const SYNTAX_APACHE_VELOCITY = 'ApacheVelocity';
+    public const SYNTAX_MAILCODE = 'Mailcode';
+
+    public static function getSupportedSyntaxes() : array
+    {
+        return array(
+            self::SYNTAX_APACHE_VELOCITY,
+            self::SYNTAX_MAILCODE
+        );
+    }
+
+    /**
+     * Retrieves the name of the syntax that the code is written in.
+     *
+     * @return string
+     * @throws Mailcode_Exception If the command has no language parameter.
+     *
+     * @see Mailcode_Commands_Command_Code::ERROR_LANG_TOKEN_MISSING
+     */
+    public function getSyntaxName() : string
+    {
+        if(isset($this->langToken))
+        {
+            return $this->langToken->getText();
+        }
+
+        throw new Mailcode_Exception(
+            'No language name available',
+            'The command has no lang token.',
+            self::ERROR_LANG_TOKEN_MISSING
+        );
+    }
+
+    // endregion
+
     /**
      * Ensures that the language token is present.
      */
     protected function validateSyntax_token() : void
     {
-        $lang = $this->params->getInfo()->getStringLiteralByIndex(0);
+        $lang = $this->params->getInfo()->getStringLiteralByIndex(1);
 
         if($lang)
         {
@@ -122,9 +159,9 @@ class Mailcode_Commands_Command_Code
         }
 
         $name = $this->langToken->getText();
-        $translator = new Mailcode_Translator();
+        $supported = self::getSupportedSyntaxes();
 
-        if($translator->syntaxExists($name))
+        if(in_array($name, $supported))
         {
             return;
         }
@@ -132,42 +169,8 @@ class Mailcode_Commands_Command_Code
         $this->validationResult->makeError(
             t('The language %1$s does not exist.', '<code>'.$name.'</code>').' '.
             t('Possible values are:').' '.
-            '<code>'.implode('</code>, <code>', $translator->getSyntaxNames()).'</code>',
+            '<code>'.implode('</code>, <code>', $supported).'</code>',
             self::VALIDATION_UNKNOWN_LANGUAGE
         );
-    }
-
-    /**
-     * Retrieves the name of the syntax that the code is written in.
-     *
-     * @return string
-     * @throws Mailcode_Exception If the command has no language parameter.
-     *
-     * @see Mailcode_Commands_Command_Code::ERROR_LANG_TOKEN_MISSING
-     */
-    public function getSyntaxName() : string
-    {
-        if(isset($this->langToken))
-        {
-            return $this->langToken->getText();
-        }
-
-        throw new Mailcode_Exception(
-            'No language name available',
-            'The command has no lang token.',
-            self::ERROR_LANG_TOKEN_MISSING
-        );
-    }
-
-    /**
-     * Retrieves an instance of the translation syntax used for
-     * the code contained in the command.
-     *
-     * @return Mailcode_Translator_Syntax
-     * @throws Mailcode_Exception
-     */
-    public function getSyntax() : Mailcode_Translator_Syntax
-    {
-        return (new Mailcode_Translator())->createSyntax($this->getSyntaxName());
     }
 }
