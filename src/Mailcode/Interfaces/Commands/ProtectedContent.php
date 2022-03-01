081a592d12129695765acc02b0d8266c728b0172
Mistralys <s.mordziol@gmail.com>
2022-03-01 18:37:13 +0100

    Added methods.

--- src/Mailcode/Interfaces/Commands/ProtectedContent.php
+++ src/Mailcode/Interfaces/Commands/ProtectedContent.php
@@ -24,7 +24,15 @@ namespace Mailcode;
  */
 interface Mailcode_Interfaces_Commands_ProtectedContent extends Mailcode_Commands_Command_Type_Standalone
 {
+    /**
+     * @see Mailcode_Traits_Commands_ProtectedContent::validateSyntax_content_id()
+     */
     public const VALIDATION_NAME_CONTENT_ID = 'content_id';
+
+    /**
+     * @see Mailcode_Traits_Commands_ProtectedContent::validateSyntax_nested_mailcode()
+     */
+    public const VALIDATION_NAME_NESTED_MAILCODE = 'nested_mailcode';
 
     public const VALIDATION_ERROR_CONTENT_ID_MISSING = 101501;
 
@@ -34,4 +42,26 @@ interface Mailcode_Interfaces_Commands_ProtectedContent extends Mailcode_Command
     public function getContentIDToken() : ?Mailcode_Parser_Statement_Tokenizer_Token_Number;
     public function getContent() : string;
     public function getContentTrimmed() : string;
+
+    /**
+     * Retrieves a collection of nested Mailcode commands.
+     *
+     * NOTE: Returns an empty collection if the nested Mailcode
+     * is not enabled (see the `code` command, which does not
+     * modify or look at its content at all).
+     *
+     * @return Mailcode_Collection
+     */
+    public function getNestedMailcode() : Mailcode_Collection;
+
+    /**
+     * Whether the content of the command may contain
+     * Mailcode, and should this be analyzed? If yes,
+     * the Mailcode will be validated (separately),
+     * and any variables will also be included in the
+     * command's variables collection.
+     *
+     * @return bool
+     */
+    public function isMailcodeEnabled() : bool;
 }
