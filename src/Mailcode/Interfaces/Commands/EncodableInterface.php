5948742ff9fa0742293ae39c80882deb488e05f6
Mistralys <s.mordziol@gmail.com>
2022-03-01 12:18:42 +0100

    Added the encodable interface.

---
+++ src/Mailcode/Interfaces/Commands/EncodableInterface.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mailcode\Interfaces\Commands;
+
+use Mailcode\Mailcode_Interfaces_Commands_Command;
+
+interface EncodableInterface extends Mailcode_Interfaces_Commands_Command
+{
+    public const ERROR_UNSUPPORTED_ENCODING = 102001;
+
+    /**
+     * @return string[]
+     */
+    public function getSupportedEncodings() : array;
+
+    public function supportsEncoding(string $keyword) : bool;
+
+    public function isEncodingEnabled(string $keyword) : bool;
+
+    /**
+     * @param string $keyword
+     * @param bool $enabled
+     * @return $this
+     */
+    public function setEncodingEnabled(string $keyword, bool $enabled) : self;
+
+    /**
+     * Retrieves the names of the currently enabled
+     * encodings (keywords), in the exact order they
+     * were specified in the command's parameters.
+     *
+     * @return string[]
+     */
+    public function getActiveEncodings() : array;
+}
---
+++ src/Mailcode/Traits/Commands/EncodableTrait.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * File containing the trait {@see \Mailcode\Traits\Commands\EncodableTrait}.
+ *
+ * @package Mailcode
+ * @subpackage Commands
+ * @see \Mailcode\Traits\Commands\EncodableTrait
+ */
+
+declare(strict_types=1);
+
+namespace Mailcode\Traits\Commands;
+
+use Mailcode\Interfaces\Commands\EncodableInterface;
+use Mailcode\Interfaces\Commands\Validation\IDNEncodeInterface;
+use Mailcode\Mailcode_Commands_Keywords;
+use Mailcode\Mailcode_Exception;
+use Mailcode\Mailcode_Interfaces_Commands_Validation_URLDecode;
+use Mailcode\Mailcode_Interfaces_Commands_Validation_URLEncode;
+
+/**
+ * Trait used to implement the encodable interface.
+ *
+ * @package Mailcode
+ * @subpackage Commands
+ * @author Sebastian Mordziol <s.mordziol@mistralys.eu>
+ *
+ * @see EncodableInterface
+ */
+trait EncodableTrait
+{
+    public function supportsEncoding(string $keyword) : bool
+    {
+        return in_array($keyword, $this->getSupportedEncodings(), true);
+    }
+
+    public function isEncodingEnabled(string $keyword) : bool
+    {
+        return $this->requireParams()
+            ->getInfo()
+            ->hasKeyword($keyword);
+    }
+
+    /**
+     * @param string $keyword
+     * @param bool $enabled
+     * @return $this
+     * @throws Mailcode_Exception
+     */
+    public function setEncodingEnabled(string $keyword, bool $enabled) : self
+    {
+        if(!$this->supportsEncoding($keyword))
+        {
+            throw new Mailcode_Exception(
+                'Cannot set encoding status, command does not support target encoding.',
+                sprintf(
+                    'Tried setting the encoding [%s], but the command only supports the following encodings: [%s].',
+                    $keyword,
+                    implode(', ', $this->getSupportedEncodings())
+                ),
+                EncodableInterface::ERROR_UNSUPPORTED_ENCODING
+            );
+        }
+
+        $this->requireParams()
+            ->getInfo()
+            ->setKeywordEnabled($keyword, $enabled);
+
+        return $this;
+    }
+
+    public function getActiveEncodings() : array
+    {
+        $keywords = $this->requireParams()
+            ->getInfo()
+            ->getKeywords();
+
+        $result = array();
+
+        // We are using the keywords list from the command's parameters
+        // here, so they are in the exact order in which they were specified.
+        foreach ($keywords as $keyword)
+        {
+            $name = $keyword->getKeyword();
+
+            if($this->supportsEncoding($name) && $this->isEncodingEnabled($name))
+            {
+                $result[] = $name;
+            }
+        }
+
+        return $result;
+    }
+
+    public function getSupportedEncodings() : array
+    {
+        $encodings = array();
+
+        if($this instanceof IDNEncodeInterface)
+        {
+            $encodings[] = Mailcode_Commands_Keywords::TYPE_IDN_ENCODE;
+        }
+
+        if($this instanceof Mailcode_Interfaces_Commands_Validation_URLEncode)
+        {
+            $encodings[] = Mailcode_Commands_Keywords::TYPE_URLENCODE;
+        }
+
+        if($this instanceof Mailcode_Interfaces_Commands_Validation_URLDecode)
+        {
+            $encodings[] = Mailcode_Commands_Keywords::TYPE_URLDECODE;
+        }
+
+        return $encodings;
+    }
+}
