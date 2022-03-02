7822c613a06c155f247ca860b0d8b7f970c98514
Mistralys <s.mordziol@gmail.com>
2022-03-02 11:12:41 +0100

    Added PHPDocs.

--- src/Mailcode/Translator/Command/ShowURLInterface.php
+++ src/Mailcode/Translator/Command/ShowURLInterface.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * File containing the interface {@see \Mailcode\Translator\Command\ShowURLInterface}.
+ *
+ * @package Mailcode
+ * @subpackage Translator
+ * @see \Mailcode\Translator\Command\ShowURLInterface
+ */
 
 declare(strict_types=1);
 
@@ -6,6 +13,13 @@ namespace Mailcode\Translator\Command;
 
 use Mailcode\Mailcode_Commands_Command_ShowURL;
 
+/**
+ * Interface for translators of the "ShowURL" command.
+ *
+ * @package Mailcode
+ * @subpackage Translator
+ * @author Sebastian Mordziol <s.mordziol@mistralys.eu>
+ */
 interface ShowURLInterface
 {
     public function translate(Mailcode_Commands_Command_ShowURL $command) : string;
