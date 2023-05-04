2ddb844f4c522aa6784c4ded9fb3a1e3e960ada3
Mistralys <s.mordziol@gmail.com>
2023-05-04 10:40:33 +0200

    Fixed same name variable tokenizing.

--- src/Mailcode/Parser/Statement/Tokenizer/Process/Variables.php
+++ src/Mailcode/Parser/Statement/Tokenizer/Process/Variables.php
@@ -10,9 +10,22 @@ class Mailcode_Parser_Statement_Tokenizer_Process_Variables extends Mailcode_Par
     {
         $vars = Mailcode::create()->findVariables($this->tokenized, $this->tokenizer->getSourceCommand())->getGroupedByHash();
 
+        $names = array();
         foreach($vars as $var)
         {
-            $this->registerToken('Variable', $var->getMatchedText(), $var);
+            $names[$var->getMatchedText()] = $var;
+        }
+
+        // Sort the variable names by longest to shortest,
+        // to ensure that $FOO does not get replaced before
+        // $FOO.BAR, which would break the second one.
+        uksort($names, static function(string $a, string $b) : int {
+            return strlen($b) - strlen($a);
+        });
+
+        foreach($names as $name => $var)
+        {
+            $this->registerToken('Variable', $name, $var);
         }
     }
 }
