2c1f943ce82666d0c30bfdc3acbb57b1f5472306
Mistralys <s.mordziol@gmail.com>
2023-06-16 13:52:23 +0200

    Namespaced test class.

--- tests/testsuites/Translator/Commands/SetVariableTests.php
+++ tests/testsuites/Translator/Commands/SetVariableTests.php
@@ -2,37 +2,40 @@
 
 declare(strict_types=1);
 
+namespace MailcodeTests\Translator\Commands;
+
 use Mailcode\Mailcode;
 use Mailcode\Mailcode_Commands_Command_SetVariable;
 use Mailcode\Mailcode_Factory;
+use VelocityTestCase;
 
-final class Translator_Velocity_SetVariableTests extends VelocityTestCase
+final class SetVariableTests extends VelocityTestCase
 {
-    public function test_translateCommand()
+    public function test_translateCommand(): void
     {
         $tests = array(
             array(
-                'label' => 'Set variable',
+                'label' => 'String value, path only variable',
                 'mailcode' => Mailcode_Factory::set()->var('FOO', 'Value'),
                 'expected' => '#set($FOO = "Value")'
             ),
             array(
-                'label' => 'Set variable',
+                'label' => 'String value, dot variable',
                 'mailcode' => Mailcode_Factory::set()->var('FOO.BAR', 'Value'),
                 'expected' => '#set($FOO.BAR = "Value")'
             ),
             array(
-                'label' => 'Set variable',
-                'mailcode' => Mailcode_Factory::set()->var('FOO.BAR', '$FOO.COUNT', true, true),
+                'label' => 'Count variable',
+                'mailcode' => Mailcode_Factory::set()->varCount('FOO.BAR', '$FOO.COUNT'),
                 'expected' => '#set($FOO.BAR = $map.of($FOO).keys("COUNT").count())'
             ),
             array(
-                'label' => 'Set variable',
+                'label' => 'Count variable with different path',
                 'mailcode' => Mailcode_Factory::set()->var('FOO.BAR', '$BER.COUNT', true, true),
                 'expected' => '#set($FOO.BAR = $map.of($BER).keys("COUNT").count())'
             ),
             array(
-                'label' => 'Set variable',
+                'label' => 'Count variable with path only',
                 'mailcode' => Mailcode_Factory::set()->var('FOO', '$BAR.COUNT', true, true),
                 'expected' => '#set($FOO = $map.of($BAR).keys("COUNT").count())'
             )
@@ -43,14 +46,14 @@ final class Translator_Velocity_SetVariableTests extends VelocityTestCase
 
     /**
      * Check the case where two same name variables are not
-     * tokenized correctly, causing a "Unquoted string literal
+     * tokenized correctly, causing an "Unquoted string literal
      * '.COUNT'" error.
      */
-    public function test_weirdParseBehavior() : void
+    public function test_weirdParseBehavior(): void
     {
         $collection = Mailcode::create()->parseString('{setvar: $FOO count: $FOO.COUNT}');
 
-        if(!$collection->isValid()) {
+        if (!$collection->isValid()) {
             $this->fail($collection->getFirstError()->getMessage());
         }
 
