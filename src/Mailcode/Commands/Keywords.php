774a10386cbea76bea5216ab93935264888cf639
Mistralys <s.mordziol@gmail.com>
2023-09-05 15:50:50 +0200

    Removed keywords replaced with named parameters.

--- src/Mailcode/Commands/Keywords.php
+++ src/Mailcode/Commands/Keywords.php
@@ -32,9 +32,6 @@ class Mailcode_Commands_Keywords
     public const TYPE_NOHTML = 'nohtml:';
     public const TYPE_ABSOLUTE = 'absolute:';
     public const TYPE_NO_TRACKING = 'no-tracking:';
-    public const TYPE_BREAK_AT = 'break-at:';
-    public const TYPE_COUNT = 'count:';
-    public const TYPE_TIMEZONE = 'timezone:';
 
     /**
      * @return string[]
@@ -52,10 +49,7 @@ class Mailcode_Commands_Keywords
             self::TYPE_IDN_DECODE,
             self::TYPE_NOHTML,
             self::TYPE_ABSOLUTE,
-            self::TYPE_NO_TRACKING,
-            self::TYPE_BREAK_AT,
-            self::TYPE_COUNT,
-            self::TYPE_TIMEZONE
+            self::TYPE_NO_TRACKING
         );
     }
 }
