eb377125ce2f2aa845e62871aebb628c396d917e
Mistralys <s.mordziol@gmail.com>
2023-09-06 12:13:08 +0200

    Added `setBreakAt()`.

--- src/Mailcode/Traits/Commands/Validation/BreakAtTrait.php
+++ src/Mailcode/Traits/Commands/Validation/BreakAtTrait.php
@@ -11,8 +11,11 @@ declare(strict_types=1);
 
 namespace Mailcode\Traits\Commands\Validation;
 
+use Mailcode\Commands\CommandException;
+use Mailcode\Commands\ParamsException;
 use Mailcode\Interfaces\Commands\Validation\BreakAtInterface;
 use Mailcode\Mailcode_Commands_Keywords;
+use Mailcode\Mailcode_Exception;
 use Mailcode\Mailcode_Parser_Statement_Tokenizer_Process_StringLiterals;
 use Mailcode\Mailcode_Parser_Statement_Tokenizer_Token;
 use Mailcode\Mailcode_Parser_Statement_Tokenizer_Token_Number;
@@ -94,5 +97,56 @@ trait BreakAtTrait
         }
 
         return null;
+    }
+
+    /**
+     * @param Mailcode_Variables_Variable|number|NULL $breakAt
+     * @return $this
+     * @throws CommandException {@see BreakAtInterface::ERROR_INVALID_BREAK_AT_VALUE}
+     * @throws ParamsException
+     */
+    public function setBreakAt($breakAt) : self
+    {
+        $info = $this->requireParams()->getInfo();
+
+        if(isset($this->breakAtToken)) {
+            $info->removeToken($this->breakAtToken);
+        }
+
+        $this->breakAtEnabled = false;
+        $this->breakAtToken = null;
+
+        if($breakAt === null) {
+            return $this;
+        }
+
+        $token = null;
+
+        if(is_numeric($breakAt))
+        {
+            $token = $info->addNumber((string)(int)$breakAt);
+        }
+        else if($breakAt instanceof Mailcode_Variables_Variable)
+        {
+            $token = $info->addVariable($breakAt);
+        }
+
+        if($token !== null)
+        {
+            $info->setParamName($token, BreakAtInterface::PARAMETER_NAME);
+
+            $this->breakAtEnabled = true;
+            $this->breakAtToken = $token;
+            return $this;
+        }
+
+        throw new CommandException(
+            'Invalid break-at value',
+            sprintf(
+                'Expected a number or variable, got: %s',
+                $breakAt instanceof Mailcode_Variables_Variable ? $breakAt->getFullName() : $breakAt
+            ),
+            BreakAtInterface::ERROR_INVALID_BREAK_AT_VALUE
+        );
     }
 }
